68b05e52c1de56895c95d3603c81a581a8372c08
Stzx <silence.m@hotmail.com>
2024-02-22 22:33:32 +0800

    nixos: nos install `boinc` & `grafana`

--- nixos/nos/misc.nix
+++ nixos/nos/misc.nix
@@ -13,6 +13,18 @@
   # for steam
   services.flatpak.enable = true;
 
+  services.boinc.enable = true;
+
+  services.grafana = {
+    enable = true;
+    settings = {
+      analytics = {
+        reporting_enabled = false;
+        feedback_links_enabled = false;
+      };
+    };
+  };
+
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false;
