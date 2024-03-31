a3f068b19ce1f8aa2ca48a3fd3bb792641346ac8
Stzx <silence.m@hotmail.com>
2024-03-31 16:09:52 +0800

    nixos: suppress warn on boot partition

--- nixos/nos/fs.nix
+++ nixos/nos/fs.nix
@@ -21,6 +21,7 @@ in
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
           };
           ORIGIN = {
