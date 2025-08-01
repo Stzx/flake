144fb2f9da9d9a04938d2b7d6f30b4628ad90f19
Stzx <silence.m@hotmail.com>
2025-08-01 10:50:00 +0800

    flake: update overlays

--- nixos/nos/overlays.nix
+++ nixos/nos/overlays.nix
@@ -23,7 +23,7 @@
   };
 
   # https://wiki.gentoo.org/wiki/AMDGPU#Firmware_blobs_for_a_known_card_model
-  linux-firmware = prev'.linux-firmware.overrideAttrs (prevAttrs: {
+  linux-firmware = prev'.linux-firmware.overrideAttrs {
     postInstall = ''
       find $out/lib/firmware/amdgpu -type f ! \( \
       -name 'sienna_cichlid_*.bin' \
@@ -34,7 +34,7 @@
       # dimgrey_cavefish_dmcub.bin, dimgrey_cavefish_vcn.bin
       # it will still point to `beige_goby`, causing the file to be copied.
     '';
-  });
+  };
 
   linuxPackages_xanmod_stable = prev'.linuxPackages_xanmod_stable.extend (
     _: prev:
@@ -103,31 +103,4 @@
     withKeePassSSHAgent = false;
     withKeePassX11 = false;
   };
-
-  # I haven't found anyone encountering a similar error in nixpkgs issues.
-  # It's strange. I fixed it myself.
-  #
-  # Module libatomic.so.1 without build-id.
-  # Module libfuse.so.2 without build-id.
-  # Stack trace of thread 416633:
-  #0  0x000077d2dbf78a5d __strlen_avx2 (libc.so.6 + 0x178a5d)
-  #1  0x0000000000409ae4 copy_exts (/nix/store/gcwvf8v46mkqdr2ndbkxmbphzh1b9fc0-avfs-1.2.0/bin/avfsd + 0x9ae4)
-  #2  0x000000000041a452 av_init_filt (/nix/store/gcwvf8v46mkqdr2ndbkxmbphzh1b9fc0-avfs-1.2.0/bin/avfsd + 0x1a452)
-  #3  0x0000000000414c04 av_init_module_uzstde (/nix/store/gcwvf8v46mkqdr2ndbkxmbphzh1b9fc0-avfs-1.2.0/bin/avfsd + 0x14c04)
-  #4  0x000000000040841f init (/nix/store/gcwvf8v46mkqdr2ndbkxmbphzh1b9fc0-avfs-1.2.0/bin/avfsd + 0x841f)
-  #5  0x0000000000403fbb open_path (/nix/store/gcwvf8v46mkqdr2ndbkxmbphzh1b9fc0-avfs-1.2.0/bin/avfsd + 0x3fbb)
-  #6  0x0000000000403e29 avfsd_getattr (/nix/store/gcwvf8v46mkqdr2ndbkxmbphzh1b9fc0-avfs-1.2.0/bin/avfsd + 0x3e29)
-  #7  0x000077d2dc134505 lookup_path (libfuse.so.2 + 0xd505)
-  #8  0x000077d2dc1347aa fuse_lib_lookup (libfuse.so.2 + 0xd7aa)
-  #9  0x000077d2dc140e6f fuse_ll_process_buf (libfuse.so.2 + 0x19e6f)
-  #10 0x000077d2dc13dac2 fuse_do_work (libfuse.so.2 + 0x16ac2)
-  #11 0x000077d2dbe97e63 start_thread (libc.so.6 + 0x97e63)
-  #12 0x000077d2dbf1bdbc __clone3 (libc.so.6 + 0x11bdbc)
-  fuse-avfs = prev'.avfs.overrideAttrs (
-    _: prevAttrs: {
-      pname = "fuse-avfs"; # I prefer the package name fuse-avfs.
-      buildInputs = (prevAttrs.buildInputs or [ ]) ++ [ final'.zstd ];
-      configureFlags = (prevAttrs.configureFlags or [ ]) ++ [ "--with-zstd=yes" ];
-    }
-  );
 })
