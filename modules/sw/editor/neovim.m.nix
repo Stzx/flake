18565f7f1c9f42ac78b50ea3164cbdce92c1ebcf
Stzx <silence.m@hotmail.com>
2025-08-31 09:42:08 +0800

    nvim: cleanup

--- modules/sw/editor/neovim.m.nix
+++ modules/sw/editor/neovim.m.nix
@@ -10,46 +10,74 @@
     let
       cfg = config.programs.neovim;
 
-      luaCfg = plugin: config: {
+      lpc = plugin: config: {
         inherit plugin config;
 
         type = "lua";
       };
 
-      nvim-twp = pkgs.vimPlugins.nvim-treesitter.withPlugins (
+      nvim-treesitter = pkgs.vimPlugins.nvim-treesitter.withPlugins (
         parser: with parser; [
           awk
-          bash
           comment
-          diff
-          kconfig
-          regex
-          udev
-          vim
-          vimdoc
-
-          asm
-          c
-          css
-          dart
-          html
-          lua
-          nasm
-          nix
-
-          cmake
           csv
-          dockerfile
+          desktop
+          diff
+          disassembly
+          dot
+          editorconfig
+          git_config
+          git_rebase
+          gitattributes
+          gitcommit
           ini
+          jq
           json
-          make
+          json5
+          kdl
+          kconfig
+          luadoc
           markdown
-          ninja
+          mermaid
+          objdump
           properties
           proto
           toml
+          udev
+          vimdoc
           xml
           yaml
+
+          asm
+          c
+          cpp
+          css
+          # dart
+          # elixir
+          # erlang
+          # go
+          # groovy
+          html
+          # java
+          # javascript
+          # kotlin
+          # latex
+          lua
+          nasm
+          nix
+          # perl
+          # python
+          # rust
+          vim
+
+          bash
+          cmake
+          dockerfile
+          make
+          meson
+          regex
+          sql
+          wit
         ]
       );
     in
@@ -62,85 +90,82 @@
           withNodeJs = false;
           withPython3 = false;
           withRuby = false;
+          extraPackages = with pkgs; [ nil ];
+          extraLuaPackages = _: [ ];
           extraLuaConfig = ''
-            local o = vim.opt
+            local opt = vim.opt
 
-            o.cursorline = true
-            o.expandtab = true
-            o.number = true
-            o.list = true
-            o.shiftwidth = 4
-            o.tabstop = 4
-            o.relativenumber = true
-            o.termguicolors = true
-            o.colorcolumn = '80'
+            opt.number = true
+            opt.relativenumber = true
+            opt.cursorline = true
+            opt.termguicolors = true
+            opt.laststatus = 3
 
-            o.swapfile = false
-            o.backup = false
-            o.writebackup = false
-
-            require("ibl").setup()
+            opt.expandtab = true
+            opt.tabstop = 4
+            opt.shiftwidth = 4
           '';
           plugins = with pkgs.vimPlugins; [
-            nvim-web-devicons
-
-            (luaCfg monokai-pro-nvim ''
-              require('monokai-pro').setup {
-                transparent_background = true,
-              }
-
-              vim.cmd([[colorscheme monokai-pro-classic]])
+            (lpc nvim-web-devicons ''
+              require('nvim-web-devicons').setup()
             '')
 
-            (luaCfg nvim-tree-lua ''
-              require('nvim-tree').setup {
+            (lpc monokai-pro-nvim ''
+              require('monokai-pro').setup({
+                  transparent_background = true,
+                  filter = 'classic',
+              })
+
+              vim.cmd([[colorscheme monokai-pro]])
+            '')
+
+            (lpc nvim-tree-lua ''
+              require('nvim-tree').setup({
                 view = {
-                  width = 50,
+                  width = 75,
                 },
-              }
+                renderer = {
+                  indent_markers = {
+                    enable = true,
+                  },
+                },
+              })
             '')
 
-            (luaCfg bufferline-nvim ''
-              require('bufferline').setup {
+            (lpc bufferline-nvim ''
+              require('bufferline').setup({
                 options = {
                   separator_style = 'slope',
                   offsets = {
-                   {
-                      filetype = 'NvimTree',
-                      text = 'File Explorer',
-                      text_align = 'center',
-                      separator = true
+                    {
+                      filetype = "NvimTree",
+                      text = "File Explorer",
+                      text_align = "center",
+                      separator = true,
                     },
                   },
                 },
-              }
+              })
             '')
 
-            (luaCfg lualine-nvim ''
-              require('lualine').setup {
+            (lpc lualine-nvim ''
+              require('lualine').setup({
                 options = {
-                  disabled_filetypes = { 'NvimTree' },
+                  theme = 'monokai-pro',
+                  disabled_filetypes = {
+                    'NvimTree',
+                  },
+                  -- extensions = { 'nvim-tree', },
                 },
-              }
+              })
             '')
 
-            (luaCfg gitsigns-nvim ''
-              require('gitsigns').setup {
-                numhl = true,
-              }
+            (lpc indent-blankline-nvim ''
+              require("ibl").setup()
             '')
 
-            indent-blankline-nvim
-
-            cmp-nvim-lsp
-            cmp-buffer
-            cmp-path
-            cmp-cmdline
-
-            nvim-lspconfig
-
-            (luaCfg nvim-twp ''
-              require('nvim-treesitter.configs').setup {
+            (lpc nvim-treesitter ''
+              require('nvim-treesitter.configs').setup({
                 auto_install = false,
                 highlight = {
                   enable = true,
@@ -148,74 +173,11 @@
                 indent = {
                   enable = true,
                 },
-              }
+              })
             '')
 
-            (luaCfg nvim-cmp ''
-              local cmp = require('cmp')
-
-              cmp.setup({
-                window = {
-                  completion = cmp.config.window.bordered(),
-                  documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                  ['<C-p>'] = cmp.mapping.select_prev_item(),
-                  ['<C-n>'] = cmp.mapping.select_next_item(),
-                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                  ['<C-Space>'] = cmp.mapping.complete(),
-                  ['<C-e>'] = cmp.mapping.abort(),
-                  ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                  { name = 'nvim_lsp' },
-                }, {
-                  { name = 'buffer' },
-                })
-              })
-
-              -- Set configuration for specific filetype.
-              cmp.setup.filetype('gitcommit', {
-                sources = cmp.config.sources({
-                  { name = 'git' },
-                }, {
-                  { name = 'buffer' },
-                })
-              })
-
-              -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-              cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                  { name = 'buffer' }
-                }
-              })
-
-              -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-              cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                  { name = 'path' }
-                }, {
-                  { name = 'cmdline' }
-                })
-              })
-
-              local caps = require('cmp_nvim_lsp').default_capabilities()
-
-              require('lspconfig').nil_ls.setup {
-                capabilities = caps,
-                -- settings = {
-                --   ['nil'] = {
-                --     formatting = {
-                --       command = { },
-                --     },
-                --   },
-                -- },
-              }
-
-              vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+            (lpc nvim-lspconfig ''
+              vim.lsp.enable('nil_ls')
             '')
           ];
         };
