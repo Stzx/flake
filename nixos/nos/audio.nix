0551768bde5e3398a05a88c54d6c82fa79583e04
Stzx <silence.m@hotmail.com>
2025-09-09 17:34:38 +0800

    nos/audio: create virtual device

--- nixos/nos/audio.nix
+++ nixos/nos/audio.nix
@@ -1,28 +1,109 @@
-{ ... }:
+{ pkgs, config, ... }:
 
+let
+  cfg = config.services.pipewire;
+in
 {
-  # qpwgraph, helvum
+  environment.systemPackages = with pkgs; [
+    # qpwgraph
 
-  services.pipewire.extraConfig.pipewire = {
-    "hw" = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.allowed-rates" = [
-          44100
-          48000
-          88200
-          96000
-          192000
-        ];
+    helvum
+  ];
+
+  services.pipewire = {
+    extraConfig = {
+      pipewire = {
+        "00-virtual-mic-sink" = {
+          "context.objects" = [
+            {
+              factory = "adapter";
+              args = {
+                "factory.name" = "support.null-audio-sink";
+
+                "node.name" = "virtual-sink";
+                "node.description" = "Virtual Sink";
+                "node.passive" = true;
+                "node.virtual" = true;
+
+                "object.linger" = true;
+
+                "media.class" = "Audio/Sink";
+
+                "audio.position" = [
+                  "FL"
+                  "FR"
+                ];
+
+                "monitor.channel-volumes" = true;
+                "monitor.passthrough" = true;
+              };
+            }
+            {
+              factory = "adapter";
+              args = {
+                "factory.name" = "support.null-audio-sink";
+
+                "node.name" = "virtual-mic";
+                "node.description" = "Virtual Microphone";
+                "node.virtual" = true;
+
+                "object.linger" = true;
+
+                "media.class" = "Audio/Source/Virtual";
+
+                "audio.position" = [
+                  "FL"
+                  "FR"
+                ];
+              };
+            }
+          ];
+        };
+        "10-virtual-mic-sink-link" = {
+          "context.exec" = [
+            {
+              path = "${cfg.package}/bin/pw-link";
+              args = [
+                "-w"
+                "virtual-sink:monitor_FL"
+                "virtual-mic:input_FL"
+              ];
+            }
+            {
+              path = "${cfg.package}/bin/pw-link";
+              args = [
+                "-w"
+                "virtual-sink:monitor_FR"
+                "virtual-mic:input_FR"
+              ];
+            }
+          ];
+        };
+
+        "99-hw" = {
+          "context.properties" = {
+            "default.clock.rate" = 48000;
+            "default.clock.allowed-rates" = [
+              44100
+              48000
+              88200
+              96000
+              192000
+            ];
+
+            "module.x11.bell" = false;
+            "module.jackdbus-detect" = false;
+          };
+        };
       };
     };
-  };
-
-  services.pipewire.wireplumber.extraConfig = {
-    "hw" = {
-      "wireplumber.profiles" = {
-        main = {
-          "hardware.bluetooth" = "disabled";
+    wireplumber.extraConfig = {
+      "99-disabled" = {
+        "wireplumber.profiles" = {
+          main = {
+            "monitor.alsa-midi" = "disabled";
+            "monitor.bluez-midi" = "disabled";
+          };
         };
       };
     };
