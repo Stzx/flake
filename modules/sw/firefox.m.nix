e0fea6e661323f307ae48fceb0a1f90b3ae562b2
Stzx <silence.m@hotmail.com>
2026-04-19 22:13:03 +0800

    firefox: update settings

--- modules/sw/firefox.m.nix
+++ modules/sw/firefox.m.nix
@@ -36,17 +36,14 @@
                   force = true;
                 };
                 settings = {
-                  "dom.security.https_only_mode" = true;
-                  "dom.security.https_only_mode_error_page_user_suggestions" = true;
-
-                  "gfx.content.skia-font-cache-size" = 32;
+                  # "gfx.webrender.debug.profiler" = true;
                   "gfx.webrender.layer-compositor" = true;
+                  "gfx.content.skia-font-cache-size" = 32;
+                  "gfx.canvas.accelerated.cache-size" = 512;
 
-                  # "layout.frame_rate" = 60;
-                  "layout.spellcheckDefault" = 0;
-
-                  "media.memory_cache_max_size" = 262144;
+                  "media.memory_cache_max_size" = 65536;
                   "media.memory_caches_combined_limit_kb" = 1048576;
+
                   "media.eme.enabled" = true;
                   "media.ffmpeg.vaapi.enabled" = true;
 
@@ -56,11 +53,24 @@
                   "image.cache.size" = 10485760;
                   "image.mem.decode_bytes_at_a_time" = 65536;
 
+                  "network.dns.disablePrefetch" = true;
+                  "network.dns.disablePrefetchFromHTTPS" = true;
+
+                  "network.trr.mode" = 5;
+                  "network.predictor.enabled" = false;
+
+                  "network.connectivity-service.enabled" = false;
+                  "network.captive-portal-service.enabled" = false;
+
+                  "layout.spellcheckDefault" = 0;
+
+                  "dom.security.https_only_mode" = true;
+                  "dom.security.https_only_mode_error_page_user_suggestions" = true;
+
                   "browser.aboutConfig.showWarning" = false;
 
                   "browser.cache.disk.enable" = false;
-                  "browser.cache.disk_cache_ssl" = false;
-                  "browser.cache.memory.capacity" = 1048576;
+                  "browser.cache.memory.capacity" = 524288;
                   "browser.cache.memory.max_entry_size" = 32768;
 
                   "browser.sessionstore.interval" = 300000;
@@ -68,7 +78,6 @@
 
                   "browser.contentblocking.category" = "standard";
 
-                  "browser.newtabpage.enabled" = false;
                   "browser.startup.homepage" = "about:blank";
                   "browser.toolbars.bookmarks.visibility" = "newtab";
 
@@ -107,32 +116,12 @@
 
                   "browser.quitShortcut.disabled" = true;
 
+                  "browser.ai.control.default" = "blocked";
+                  "browser.ml.enable" = false;
                   "browser.ml.chat.enabled" = false;
-
-                  # "network.dns.disableIPv6" = true;
-                  "network.dns.disablePrefetch" = true;
-                  "network.dns.disablePrefetchFromHTTPS" = true;
-
-                  "network.trr.mode" = 5;
-                  "network.predictor.enabled" = false;
-
-                  "network.connectivity-service.enabled" = false;
-                  "network.captive-portal-service.enabled" = false;
-
-                  # "sidebar.visibility" = "expand-on-hover";
-                  "sidebar.position_start" = false;
-                  "sidebar.main.tools" = "history,bookmarks";
-                  # "sidebar.verticalTabs" = true;
-
-                  "privacy.partition.serviceWorkers" = true;
-
-                  # PASSWORDS
-                  "signon.autofillForms" = false;
-                  "signon.rememberSignons" = false;
-                  "signon.formlessCapture.enabled" = false;
-                  "signon.firefoxRelay.feature" = "disabled";
-                  "signon.generation.enabled" = false;
-                  "signon.management.page.breach-alerts.enabled" = false;
+                  "browser.ml.chat.menu" = false;
+                  "browser.ml.linkPreview.enabled" = false;
+                  "browser.tabs.groups.smart.enabled" = false;
 
                   "services.sync.log.appender.file.logOnError" = false;
                   "services.sync.log.appender.file.logOnSuccess" = false;
@@ -140,14 +129,29 @@
                   "extensions.formautofill.addresses.enabled" = false;
                   "extensions.formautofill.creditCards.enabled" = false;
                   "extensions.htmlaboutaddons.recommendations.enabled" = false;
-
                   "extensions.pocket.enabled" = false;
+                  "extensions.getAddons.cache.enabled" = false;
 
+                  "devtools.cache.disabled" = true;
                   "devtools.debugger.skip-pausing" = true;
                   "devtools.aboutdebugging.showHiddenAddons" = true;
 
+                  # "sidebar.visibility" = "expand-on-hover";
+                  # "sidebar.position_start" = false;
+                  # "sidebar.main.tools" = "history,bookmarks";
+                  # "sidebar.verticalTabs" = true;
+
                   "app.normandy.enabled" = false;
                   "app.normandy.first_run" = false;
+                  "app.normandy.api_url" = "";
+
+                  "signon.autofillForms" = false;
+                  "signon.rememberSignons" = false;
+                  "signon.formlessCapture.enabled" = false;
+                  "signon.firefoxRelay.feature" = "disabled";
+                  "signon.generation.enabled" = false;
+                  "signon.management.page.breach-alerts.enabled" = false;
+                  "signon.privateBrowsingCapture.enabled" = false;
 
                   "pdfjs.defaultZoomValue" = "page-fit";
                   "pdfjs.enabledCache.state" = true;
@@ -160,9 +164,20 @@
                   "trailhead.firstrun.branches" = "nofirstrun-empty";
                   "trailhead.firstrun.didSeeAboutWelcome" = true;
 
+                  "privacy.partition.serviceWorkers" = true;
+
                   "toolkit.telemetry.enabled" = false;
                   "toolkit.telemetry.archive.enabled" = false;
                   "toolkit.telemetry.unified" = false;
+                  "toolkit.telemetry.server" = "data:,";
+                  "toolkit.telemetry.newProfilePing.enabled" = false;
+                  "toolkit.telemetry.shutdownPingSender.enabled" = false;
+                  "toolkit.telemetry.updatePing.enabled" = false;
+                  "toolkit.telemetry.bhrPing.enabled" = false;
+                  "toolkit.telemetry.firstShutdownPing.enabled" = false;
+                  "toolkit.telemetry.coverage.opt-out" = true;
+                  "toolkit.coverage.opt-out" = true;
+                  "toolkit.coverage.endpoint.base" = "";
 
                   "datareporting.usage.uploadEnabled" = false;
                   "datareporting.healthreport.uploadEnabled" = false;
