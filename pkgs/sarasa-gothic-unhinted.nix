c047c64e9e2d7807dd4e21bc9ea0e4fb7fd19f68
Stzx <silence.m@hotmail.com>
2026-07-29 22:50:41 +0800

    sarasa-gothic-unhinted: 1.0.37 -> 1.0.40

--- pkgs/sarasa-gothic-unhinted.nix
+++ pkgs/sarasa-gothic-unhinted.nix
@@ -8,11 +8,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "sarasa-gothic-unhinted";
-  version = "1.0.37";
+  version = "1.0.40";
 
   src = fetchurl {
     url = "https://github.com/be5invis/Sarasa-Gothic/releases/download/v${finalAttrs.version}/Sarasa-TTC-Unhinted-${finalAttrs.version}.7z";
-    hash = "sha256-oRIyCMitp8awbzXZJ8p+U5AOm+Y3QsTxPdBVTy3Fu4o=";
+    hash = "sha256-vI3W1jetphMwJyw06A3mn+23c5pjZ0EJErr17proB1A=";
   };
 
   sourceRoot = ".";
